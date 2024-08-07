b5d7da19631a18518022feaab1bceeb6f59a70f6
rstringa <robertostringa@hotmail.com>
2024-08-07 19:42:39 -0300

    added actions

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,18 +7,22 @@ import icon from "astro-icon";
 const SERVER_PORT = 4321;
 const LOCALHOST_URL = `http://localhost:${SERVER_PORT}`;
 
-const LIVE_URL = "https://astroship.web3templates.com";
+const LIVE_URL = "https://github.com/rstringa.github.io";
 const SCRIPT = process.env.npm_lifecycle_script || "";
 const isBuild = SCRIPT.includes("astro build");
+
+let SITE_URL = LOCALHOST_URL;
 export let BASE_URL = LOCALHOST_URL;
 
 if(isBuild){
-  BASE_URL = LIVE_URL
+  SITE_URL = LIVE_URL;
+  BASE_URL = '/Astro4';
 }
 
 // https://astro.build/config
 export default defineConfig({
   server : {port:SERVER_PORT },
-  site: BASE_URL,
+  site: SITE_URL,
+  base: BASE_URL,
   integrations: [tailwind(), mdx(), sitemap(), icon()],
 });
