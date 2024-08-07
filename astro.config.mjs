fa9164e303b4aab0738e154273f6ee2f74434372
rstringa <robertostringa@hotmail.com>
2024-08-07 20:20:39 -0300

    ...

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import icon from "astro-icon";
 const SERVER_PORT = 4321;
 const LOCALHOST_URL = `http://localhost:${SERVER_PORT}`;
 
-const LIVE_URL = "https://github.com/rstringa/Astro4.github.io";
+const LIVE_URL = "https://rstringa.github.io";
 const SCRIPT = process.env.npm_lifecycle_script || "";
 const isBuild = SCRIPT.includes("astro build");
 
@@ -16,12 +16,13 @@ export let BASE_URL = ""
 
 if(isBuild){
   SITE_URL = LIVE_URL;
-  BASE_URL = '/Astro4';
+  BASE_URL = '/Astro4.github.io';
 }
 
 // https://astro.build/config
 export default defineConfig({
   server : {port:SERVER_PORT },
   site: SITE_URL,
+  base : BASE_URL,
   integrations: [tailwind(), mdx(), sitemap(), icon()],
 });
