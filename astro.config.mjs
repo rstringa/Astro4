d62973e7089a4dc1dd7a31df17a4eec010ad718b
rstringa <robertostringa@hotmail.com>
2024-08-07 19:46:32 -0300

    astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import icon from "astro-icon";
 const SERVER_PORT = 4321;
 const LOCALHOST_URL = `http://localhost:${SERVER_PORT}`;
 
-const LIVE_URL = "https://github.com/rstringa.github.io";
+const LIVE_URL = "https://rstringa.github.io";
 const SCRIPT = process.env.npm_lifecycle_script || "";
 const isBuild = SCRIPT.includes("astro build");
 
