e6c0e8f74f8f88b067597a458d96b2f0f46064f7
rstringa <robertostringa@hotmail.com>
2024-08-18 19:08:44 -0300

    ....

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,20 +4,20 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import icon from "astro-icon";
 
-const SERVER_PORT = 4321;
-const LOCALHOST_URL = `http://localhost:${SERVER_PORT}`;
+// const SERVER_PORT = 4321;
+// const LOCALHOST_URL = `http://localhost:${SERVER_PORT}`;
 
-const LIVE_URL = "https://github.com/rstringa/Astro4";
-const SCRIPT = process.env.npm_lifecycle_script || "";
-const isBuild = SCRIPT.includes("astro build");
+// const LIVE_URL = "https://github.com/rstringa/Astro4";
+// const SCRIPT = process.env.npm_lifecycle_script || "";
+// const isBuild = SCRIPT.includes("astro build");
 
-export let SITE_URL = LOCALHOST_URL;
-export let BASE_URL = '/';
+// export let SITE_URL = LOCALHOST_URL;
+ export let ROOT = '';
 
-if(isBuild){
-  SITE_URL = LIVE_URL;
-  // BASE_URL = '/dist';
-}
+// if(isBuild){
+//   SITE_URL = LIVE_URL;
+//   // BASE_URL = '/dist';
+// }
 
 // https://astro.build/config
 export default defineConfig({
@@ -28,13 +28,9 @@ export default defineConfig({
   //   format: "file",
   // },
   // outDir: '.'+ BASE_URL,
-  base: BASE_URL,
-  build: {
-    rollupOptions: {
-      external: ['fsevents']
-    }
-  },
-  site: SITE_URL,
+  // base: BASE_URL,
+
+  site: "https://github.com/rstringa/Astro4",
   integrations: [tailwind(), mdx(), sitemap(), icon()],
  
 });
